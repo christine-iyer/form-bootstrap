afedc96320edaa9965837278bc63013fe8e1af81
chrisIyer <hellohotbox420@gmail.com>
2023-04-02 12:15:43 -0400

    new form.

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import './App.css';
+import BasicExample from './components/BasicExample';
+import FormDisabledInputExample from './components/FormDisabledInputExample';
+import TextControlsExample from './components/TextControlExmple';
+import InputSizesExample from './components/InputSizesExample';
+import FormFileExample from './components/FormFileEzample';
+import SelectSizesExample from './components/SelectSizesExample'
+import ChecksRadios from './components/ChecksRadios';
+import RangeExample from './components/RangeExample';
+import InputGroupEx from './components/InputGroup';
+import GridComplexExample from './components/GridComplexExample';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+         
+        </p>
+       <BasicExample />
+       <FormDisabledInputExample />
+       <TextControlsExample />
+       <InputSizesExample />
+       <FormFileExample />
+       <SelectSizesExample />
+       <ChecksRadios />
+       < RangeExample />
+       <InputGroupEx />
+       <GridComplexExample />
+      </header>
+    </div>
+  );
+}
+
+export default App;
